0438047f0eb77dddb59335d59c015980a8589a2b
johntronik <yutaro.saito@estie.co.jp>
2024-05-21 16:59:14 +0900

    add subset_option

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ import matplotlib_fontja  # noqa
 import streamlit as st
 
 st.set_page_config(page_title="ベン図を描くアプリ", page_icon=":bar_chart:")
-st.title("ベン図を描こう")
+st.title("ベン図描く")
 st.markdown(
     "[matplotlib-venn](https://github.com/konstantint/matplotlib-venn) を使ってベン図を描くアプリです。"
 )
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    left = st.number_input("左集合の数", step=1, value=1)
+    left_input = st.number_input("←左集合の数", step=1, value=3)
     left_label = st.text_input("左集合のラベル", "Left")
 with col2:
     common = st.number_input("共通集合の数", step=1, value=2)
 with col3:
-    right = st.number_input("右集合の数", step=1, value=3)
+    right_input = st.number_input("右集合の数→", step=1, value=5)
     right_label = st.text_input("右集合のラベル", "Right")
+
+is_subset = st.toggle("共通部分を左右の集合から引く", value=True)
+left = left_input - common * is_subset
+right = right_input - common * is_subset
 
 with st.expander("詳細設定"):
     colx, coly = st.columns(2)
